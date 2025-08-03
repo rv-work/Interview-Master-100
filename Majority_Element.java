4956135b2564b313d39d8a350ce78d11a4783a44
rv-work <rv.dthought@gmail.com>
2025-08-03 19:49:52 +0530

    Day12

---
+++ Majority_Element.java
@@ -0,0 +1,19 @@
+class Solution {
+  public int majorityElement(int[] nums) {
+    int cnt = 0;
+    int el = -1;
+    ;
+    for (int i = 0; i < nums.length; i++) {
+      if (cnt == 0) {
+        cnt = 1;
+        el = nums[i];
+      } else if (nums[i] == el) {
+        cnt++;
+      } else {
+        cnt--;
+      }
+
+    }
+    return el;
+  }
+}
